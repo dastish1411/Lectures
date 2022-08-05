19cc1b9f680cb000c5dad412d1898d77a032f995
dastish1411 <dastisch123@gmail.com>
2022-08-05 19:58:45 +0600

    python

--- python_start/dict.py
+++ python_start/dict.py
@@ -1,126 +1,121 @@
-""" Cловари """
+""" Cловари (dict) """
 
-# Cловарь - изменяемый, итерируемый. Состоят из пар: 
-# ключ: значение. Ключи могут быть только неизменяемые типы данных 
-# (tuple, str, int, None, bool), а значения - любые 
- 
- 
-# dict_ = {} 
-# passport = { 
-#     'name': 'Bilal', 
-#     'last_name':'Abd', 
-#     'age': 19, 
-#     'gender': 'pokemon', 
-#     'birthday': '23.02.2003' 
-#     } 
- 
-# print(passport['name']) 
-# print(passport['last_name']) 
-# print(passport['age']) 
-# print(passport['gender']) 
-# print(passport['birthday']) 
- 
-# dict2 = dict(name = 'Aman', last_name = 'Bol', age = 19, gender = 'pokemon', birthday = '23.02.2003') 
- 
-# dict3 = dict.fromkeys(['a', 'b'], 25) 
-# print(passport) 
-# print(dict_) 
-# print(dict2) 
-# print(dict3) 
- 
-# dict4 = dict.fromkeys(['a', '5']) # ([], None) 
- 
-# dict5 = dict([('name', 'Moon'), ('last_name', 'light'), ('age', 23), ('gender', 'pokemon'), ('birthday', '23.03.2003')]) 
- 
- 
- 
-'''----''' 
- 
- 
-# dict_ = {} 
-# passport = { 
-#     'name': 'Bilal', 
-#     'last_name':'Abd', 
-#     'age': 19, 
-#     'gender': 'pokemon', 
-#     'birthday': '23.02.2003' 
-#     } 
- 
-# print(passport.get('name')) 
-# print(passport.get('ida')) 
-# dict.get(key, None) - щтдает значение указанного ключа, 
-# если нет _ отдает второе указанное значение 
-# (по умолчанию) 
-# print(passport.get('id', 'Нет такого ключа')) 
- 
-# passport.setdefault(key, None) - отдает занчение указанного ключа 
-# если его нет - создает со значением dafault(по умолчанию None) 
-# passport.setdefault('age') # 23 
-# print(passport.setdefault('id', 34444)) 
-# print(passport) 
- 
-# passport.update({key: value}) - принимает в себя другой словарь и  
-# обновляет исходный за счет него 
- 
- 
-# dict8 = { 
-#     'name': 'Lalib', 
-#     'last_name':'Abd', 
-#     'age': 19, 
-#     'gender': 'pokemon', 
-#     'birthday': '23.02.2003'  
-#     } 
-# passport.update(dict8) 
-# print(passport) 
- 
- 
-# a = {'a': 11, 'b': 22} 
-# a['c'] = 33 
-# print(a) 
- 
- 
- 
-# dict10 = { 
-#     'name': 'Lalib', 
-#     'last_name':'Abd', 
-#     'age': 19, 
-#     'gender': 'pokemon', 
-#     'birthday': '23.02.2003'  
-#     } 
-# dict10.pop('name') 
-# print(dict10) 
- 
-# print(dict10.pop('id', 'Нет такого ключа')) 
-# print(dict10) 
- 
-# it = dict10.items() 
-# print(it) 
-# ''' Методы ''' 
- 
-# get(key, None) 
- 
-# clear() 
- 
-# copy() 
- 
-# pop(key, default) 
- 
-# popitem() 
- 
-# setdefault(key, default) 
- 
-# update() 
- 
-# dict1 = {1: 'Tom', 2: 'Alice'} 
-# dict2 = {1: 'Tom', 2: 'Alice'} 
- 
-# fromkeys(seq, value = None) 
-  
-# nums = [1,2,3,4,5] 
-# new_d = dict.fromkeys(nums, 'M') 
-# print(new_d) 
- 
-# items(), keys(), values() 
+# # Словарь - изменяемый, итерируемый. Состоят из пар: ключ: значение. Ключами могут быть только неизменяемые типы данных(tuple, str, int, None, bool), а значениями - любые. Ключи должны быть уникальными
+
+# dict_ = {}
+# # passport = {'name': 'Айбек', 'last_name': 'Абдулаев', 'age': 23, 'gender': 'M', 'birthday': '13.06.1997'}
+
+# # print(passport['name']) # 'Айбек'
+# # print(passport['age']) # 23
+# # print(passport[0]) # KeyError
+
+# dict2 = dict(name='Барсбек', last_name='Михаилов', age=19)
+# print(dict2) # {'name': 'Барсбек', 'last_name': 'Михаилов', 'age': 19}
+
+# dict3 = dict.fromkeys(['a', 'b'], 25)
+# print(dict3) # {'a': 25, 'b': 25}
+
+# dict4 = dict.fromkeys(['a', 'b'])
+# print(dict4) # {'a': None, 'b': None}
+
+# dict5 = dict([('name', 'John'), ('last_name', 'Watson')])
+# print(dict5)
+# print(dict5['name'])
+
+""" Методы словарей """
+passport = {'name': 'Айбек', 'last_name': 'Абдулаев', 'age': 23, 'gender': 'M', 'birthday': '13.06.1997'}
+
+# passport['name'] # "Айбек"
+# passport['ID'] # KeyError
+
+# print(passport.get('name')) # "Айбек"
+# print(passport.get('ID')) # None
+
+# dict.get(key, None) - отдает значение указанного ключа, если нет - отдает второе указанное значение(по умолчанию None)
+
+# print(passport.get('ID', 'Нет такого ключа!')) # 'Нет такого ключа!'
+
+
+# passport.setdefault(key, default) - отдает значение указанного ключа, если его нет - создает его со значением default(по умолчанию - None)
+
+# print(passport.setdefault('age')) # 23
+# print(passport.setdefault('ID')) # None
+# print(passport.setdefault('ID', 5234524))
+# print(passport)
+
+
+# passport.update({key: value}) - принимает в себя другой словарь и обновляет исходный словарь за счет него
+
+# dict8 = {'name': 'Майрам', 'age': 27, 'name': 'Азамат', 'name': 'Кайрат', 'ID': 43781623}
+# passport.update(dict8)
+# print(passport)
+# print(dict8)
+
+
+# a = {'a': 10, 'b': 20}
+# a['c'] = 30
+# a['b'] = 40
+# print(a) # {'a': 10, 'b': 40, 'c': 30}
+
+
+dict10 = {'name': 'Алия', 'last_name': 'Азизова', 'age': 21}
+# dict10.pop('name')
+# print(dict10)
+
+# deleted_el = dict10.pop('ID', 'Нет такого ключа!')
+# print(deleted_el) # 'Нет такого ключа!'
+# print(dict10)
+
+# deleted_el = dict10.popitem()
+# print(deleted_el)
+# print(dict10)
+
+# dict10.clear()
+# print(dict10) # {}
+
+# del dict10['age']
+# print(dict10)
+
+
+
+iter_dict = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+# for i in iter_dict:
+#     print(i) # keys
+
+# for i in iter_dict:
+#     print(iter_dict[i]) # values
+
+
+""" keys(), values(), items() """
+k = iter_dict.keys()
+# print(k)
+# for key in k:
+#     print(key)
+
+v = iter_dict.values()
+# print(v)
+# for value in v:
+#     print(value)
+
+it = iter_dict.items()
+# print(it)
+# for key, value in it:
+#     print(f'ключи {key}, значения {value}')
+
+
+# contacts = {
+#     'names': {
+#         'Aidar': 'asdfasdf',
+#         'Baatai': 9967357223,
+#         'Igor': 99635272839
+#     }
+# }
+# # print(contacts['names']['Igor'])
+# names = contacts['names']
+# print(names['Igor'])
+
+# copy() - копирует словарь
+# contacts_copy = contacts.copy()
 
 # from string import ascii_lowercase
 
@@ -154,3 +149,4 @@
 #     for i in range(len(v)):
 #         list_.append(v[i] + '@' + k)
 # print(list_)    
+
